d886c699ef8005b5945ab2b3f3ac9baf89401121
Imran Ali <imranalishahani7@gmail.com>
2023-02-06 12:16:12 +0500

    Name Change of Joel

--- indexOf lastIndexOf.js
+++ indexOf lastIndexOf.js
@@ -2,7 +2,7 @@ let a = ["Safiullah", "Jawad", "Zeeshan", "Yaseen","Ab.Hussain"];
  b= a.indexOf("Zeeshan",1);
 console.log(b + "<br><br>");
 
-let c =["Ali","Joal","Salman","Abdul"];
+let c =["Ali","Joel","Salman","Abdul"];
 
 d = c.lastIndexOf("Joal", 3);
 console.log(d);
